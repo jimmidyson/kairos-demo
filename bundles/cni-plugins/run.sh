240630851c4c38f302e5eabf16d5692618fac796
Shalin Patel <shalin.patel@nutanix.com>
2025-12-19 15:18:27 -0800

    fix: paths for binaries

--- bundles/cni-plugins/run.sh
+++ bundles/cni-plugins/run.sh
@@ -2,4 +2,4 @@
 
 mkdir -p /usr/local/.state/opt.bind/nkp/cni/bin
 
-cp -rfv ./opt/cni/bin/ /usr/local/.state/opt.bind/nkp/cni/bin/
+cp -rfv ./opt/cni/bin/ /usr/local/.state/opt.bind/nkp/cni/
--- bundles/kubernetes/run.sh
+++ bundles/kubernetes/run.sh
@@ -2,4 +2,4 @@
 
 mkdir -p /usr/local/.state/opt.bind/nkp/kubernetes/bin
 
-cp -rfv ./opt/kubernetes/bin/ /usr/local/.state/opt.bind/nkp/kubernetes/bin/
+cp -rfv ./opt/kubernetes/bin/ /usr/local/.state/opt.bind/nkp/kubernetes
